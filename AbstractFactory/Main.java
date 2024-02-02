ef4a3392af8459cc72bee18ff4cab94519090697
MKRadius <caomanhkhai110@gmail.com>
2024-02-02 05:57:52 +0200

    AbstractFactory

--- AbstractFactory/Main.java
+++ AbstractFactory/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        UIFactory factory = new BFactory();
+        UIFactory factory = new AFactory();
+        // UIFactory factory = new BFactory();
 
-        Button button       = factory.createButton("Button-A");
-        TextField textField = factory.createTextField("TextField-A");
-        Checkbox checkbox   = factory.createCheckbox("Checkbox-A");
+        Button button       = factory.createButton("Click me");
+        TextField textField = factory.createTextField("Enter something here");
+        Checkbox checkbox   = factory.createCheckbox("Mark this box");
         
         button.display();
         textField.display();
         checkbox.display();
 
-        button.setText("New Button A");
-        textField.setText("New TextField A");
-        checkbox.setText("New Checkbox A");
+        // Change the text of the components
+        button.setText("New Button");
+        textField.setText("New TextField");
+        checkbox.setText("New Checkbox");
 
         button.display();
         textField.display();
