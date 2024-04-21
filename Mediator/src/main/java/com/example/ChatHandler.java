91772f1bb67a24228d089cea2b818a90af0d86b9
MKRadius <caomanhkhai110@gmail.com>
2024-04-21 21:48:41 +0300

    Mediator

--- Mediator/src/main/java/com/example/ChatHandler.java
+++ Mediator/src/main/java/com/example/ChatHandler.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ChatHandler  {
+public class ChatHandler implements Mediator {
     private HashMap<Client, Window> clientWindow;
 
     public ChatHandler() {
